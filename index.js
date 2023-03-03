d3a000faef08a8cf59d862aa15f936cdbabfdc13
Rusty12321 <weston@hilgenfeld.org>
2023-03-03 16:25:28 -0600

    solution

--- index.js
+++ index.js
@@ -1,5 +1,37 @@
-(function() {
-  'use strict';
+  let stopButton = document.getElementById("stopButton");
+  let slowButton = document.getElementById("slowButton");
+  let goButton = document.getElementById("goButton");
+  let stopLight = document.getElementById("stopLight");
+  let slowLight = document.getElementById("slowLight");
+  let goLight = document.getElementById("goLight");
 
-  // YOUR CODE HERE
-})();
+  stopButton.addEventListener("click", function() {
+    stopLight.classList.toggle("stop");
+  });
+
+  slowButton.addEventListener("click", function() {
+    slowLight.classList.toggle("slow");
+  });
+
+  goButton.addEventListener("click", function() {
+    goLight.classList.toggle("go");
+  });
+
+  let buttons = document.querySelectorAll('.button');
+
+  for (i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('mouseenter', function(event) {
+      buttonLogEnter(event.target.innerText);
+    })
+    buttons[i].addEventListener('mouseleave', function(event) {
+      buttonLogLeave(event.target.innerText);
+    })
+  }
+
+function buttonLogEnter(buttonName) {
+  console.log(`Enter ${buttonName} button`);
+}
+
+function buttonLogLeave(buttonName) {
+  console.log(`Left ${buttonName} button`)
+}
